f0bc754968420879fb5fd45ae2fa5eaa3decd193
Paulo Tolentino <paulo.sergio1001@hotmail.com>
2023-11-03 17:34:43 -0300

    refactor: move success http response to a factory method

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from "../../errors";
-import { badRequest, serverError } from "../../helpers/http-helper";
+import { badRequest, ok, serverError } from "../../helpers/http-helper";
 import {
   Controller,
   EmailValidator,
@@ -40,10 +40,7 @@ export class SignUpController implements Controller {
       }
 
       const account = this.addAccount.add({ email, password, name });
-      return {
-        statusCode: 200,
-        body: account,
-      };
+      return ok(account);
     } catch (error) {
       return serverError();
     }
