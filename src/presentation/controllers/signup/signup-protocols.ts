57acb382bd0fd7bf4e85df70bfb93b2c7543cf2f
Paulo Tolentino <paulo.sergio1001@hotmail.com>
2023-11-03 21:11:20 -0300

    refactor: change folder name

--- src/presentation/controllers/signup/signup-protocols.ts
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -1,4 +1,4 @@
 export * from "../../protocols";
 export * from "../../protocols/email-validator";
-export * from "../../../domain/use-cases";
+export * from "../../../domain/usecases";
 export * from "../../../domain/models";
